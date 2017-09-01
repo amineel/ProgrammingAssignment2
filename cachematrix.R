0a54ed50616471ec4316c04f2557df7de55473a8
EL AJI Mohamed Amine SOFRECOM <BRNR6700@orange.com>
2017-08-31 21:21:33 -0400

    Programm assignement 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,39 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(M = matrix()) {
+	##initiate the inverse of the matrix and set the value of the matrix
+	I<-NULL  
+    set <- function(Y=matrix()) {
+       ##set the values in parent environnement
+   	    M <<- Y
+        I <<- NULL
+    }
+    get <- function() M
+	##set the inverse in parent environnement
+    setInverse <- function(inverse) I <<- inverse
+    getInverse <- function() I
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(l, ...) {
+    ## Return a matrix that is the inverse of 'x'
+	## getting the inverse stored in the list
+	I <- l$getInverse()
+	##check if the inverse is not null and return it, otherwise make the calculation
+    if(!is.null(I)) {
+        message("getting cached data")
+        return(I)
+    }
+    M <- l$get()
+    I <- solve(M)
+    l$setInverse(I)
+    I	
+		
 }
